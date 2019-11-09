287035dcd307a7ffa8004f68296a2dc36fffc6ee
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-11-09 11:40:29 +0000

    tidying and refactoring

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using Unity.Mathematics;
-using Unity.Collections;
 
 public class Sphere : MonoBehaviour
 {
@@ -9,51 +8,51 @@ public class Sphere : MonoBehaviour
 
     const float radians = 6.283f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if(gridSize < 1)
-            gridSize = 1;
+        gridSize = math.clamp(gridSize, 1, 100);
+        radius = math.clamp(radius, 0, 20);
 
+        PlotPointsOnSphere();
+    }   
+
+    void PlotPointsOnSphere()
+    {
         float thetaIncrement = (gridSize / radius) / math.PI;
         int pointCount = (int)(math.PI / thetaIncrement);
 
         for(int i = 0; i < pointCount; i++)
         {
             float theta = thetaIncrement * i + (thetaIncrement * 0.5f); 
-            Lines(theta);
+            PlotHorizontalRing(theta);
         }
     }
 
-    void Lines(float theta)
+    void PlotHorizontalRing(float theta)
     {   
-        float phiRadius = radius * math.sin(theta);
+        float ringRadius = radius * math.sin(theta);
 
-        float phiIncrement = (gridSize / phiRadius) / math.PI;
+        float phiIncrement = (gridSize / ringRadius) / math.PI;
         int pointCount = (int)(math.PI*2 / phiIncrement);
 
         for(int i = 0; i < pointCount; i++)
         {
             float phi = phiIncrement * i + (phiIncrement * 0.5f);
-            Line(theta, phi);
+            DrawLine(theta, phi);
         }
     }
 
-    void Line(float theta, float phi)
+    void DrawLine(float theta, float phi)
     {
         float3 position = PositionOnSphere(theta, phi);
-        Debug.DrawLine(float3.zero + (position*0.8f), position, new Color(position.x, position.y, position.z));
+        Debug.DrawLine(
+            float3.zero + (position*0.8f),
+            position, new Color(position.x,
+            position.y, position.z));
     }
 
     float3 PositionOnSphere(float theta, float phi)
     {
-        float degTheta = theta;
-        float degPhi = phi;
         float x = radius * math.sin(theta) * math.cos(phi);
         float y = radius * math.cos(theta);
         float z = radius * math.sin(theta) * math.sin(phi);
